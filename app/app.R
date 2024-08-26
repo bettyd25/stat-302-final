49f975a6d382bb38a30a03ce794febd81834f98d
Betty <bettydong2025@u.northwestern.edu>
2024-08-25 22:39:57 -0500

    finished shiny app

--- app/app.R
+++ app/app.R
@@ -7,6 +7,8 @@ library(sf)
 library(gt)
 
 # loading data---------------
+load("data/merged_kpop_hits_all_years.rds") 
+
 load("data/shiny_kpop_hits_data.rds")
 load("data/top_kpop_groups.rds")
 
@@ -57,26 +59,56 @@ ui <- page_sidebar(
         "artist_choice",
         label = strong("Select Kpop Artist of Interest"),
         choices = artist_names
-      )
+      ),
+      br(),
+      tableOutput("songTable"),
+      
+      br(),
+      
+      tableOutput("memberTable"),
+      card_body("Note that some groups may not have members listed due to their current status."),
+      br(),
+      plotOutput("agePlot")
     ),
+    
+
+      card(
+        imageOutput("groupImage"),
+        card_body("The color is the color or fan color associated with the kpop artist!")
+        ),
+    
     layout_column_wrap(
       width = 0.5,
       
       card(
-        plotOutput("polarPlot"),
-        card_body("The color is the color or fan color associated with the kpop artist!")
+        plotOutput("polarPlot")
+        
       ),
       
       card(
-        tableOutput("memberTable"),
-        card_body("Note that some groups may not have members listed due to their current status.")
+        selectInput(
+          "song_aspect",
+          label = "Select aspect of songs",
+          
+          choices = list(
+            "Danceability",
+            "Loudness",
+            "Energy",
+            "Acousticness",
+            "Mode",
+            "Speechiness",
+            "Tempo",
+            "Duration (ms)"
+          )
+           
+        ),
+        
+        plotOutput("songPlot")
+        
+        
+          )
+        )
       )
-    ),
-    card(
-      imageOutput("groupImage")
-    )
-   
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -94,7 +126,7 @@ server <- function(input, output) {
       filter(artist.s == artist, variable != "avg_year") |>
       ggplot(mapping = aes(x = variable, 
                            y = value)) +
-      geom_col(width = 1, alpha = 0.5,
+      geom_col(width = 1,
                fill = artist_color) +
       geom_hline(yintercept = seq(1, 10, 1), linetype = "dotted") +
       geom_segment(
@@ -181,6 +213,122 @@ server <- function(input, output) {
     )
   
   }, deleteFile = FALSE)
+  
+  # make plots of member ages
+  output$agePlot <- renderPlot({
+    selected_group <- tolower(input$artist_choice)
+    
+    artist_color <- artist_hex_codes |> filter(
+      artist_name == input$artist_choice
+    ) |> 
+      pull(hex_code)
+    
+    
+    member <- top_kpop_idols |> 
+      filter(
+        selected_group == Group |
+          selected_group == `Former Group` |
+          selected_group == `Other Group`
+      ) |> 
+      mutate(age = as.numeric(difftime(today(), dmy(`Date of Birth`), units = "days")) / 365.25) |> 
+      mutate(age = round(age, 0))
+    
+    if (nrow(member) == 0) {
+      return(
+        ggplot() +
+          geom_blank() +
+          theme_void() +
+          labs(title = "No age boxplot available")
+      )
+    }
+    
+    ggplot(member, aes(x = age)) +
+      geom_boxplot(fill = artist_color) +
+      scale_x_continuous(
+        name = NULL,
+        breaks = seq(0, ceiling(max(member$age)), by = 1),
+        labels = function(x) paste0(x, " years")  
+      ) +
+      labs(
+        title = "Boxplot of Member Ages"
+      ) +
+      coord_flip() +
+      theme_minimal() +
+      theme(
+        axis.text.y = element_text(angle = 0, hjust = 0.5), 
+        axis.title.y = element_blank(), 
+        axis.text.x = element_blank()
+      )
+  })
+  
+  # make table of top songs
+  output$songTable <- render_gt({
+    artist <- input$artist_choice
+    
+    artist_color <- artist_hex_codes |> filter(
+      artist_name == input$artist_choice
+    ) |> 
+      pull(hex_code)
+    
+    merged_kpop_hits_all_years |> 
+      filter(artist.s == artist) |> 
+      select(title, year) |> 
+      gt() |> 
+      tab_header(
+        title = paste("Top Songs of", artist),
+        subtitle = "Top Songs in Years"
+      ) |> 
+      tab_style(
+        style = cell_text(align = "center"),
+        locations = cells_body(columns = everything())
+      ) |> 
+      tab_style(
+        style = cell_text(align = "center", weight = "bold"),
+        locations = cells_column_labels(columns = everything())
+      ) |> 
+      tab_options(
+        table.background.color = artist_color
+      )
+  }) 
+  
+  # make toggle-able year boxplots
+  
+  output$songPlot <- renderPlot({
+    artist <- input$artist_choice
+    
+    artist_color <- artist_hex_codes |> filter(
+      artist_name == input$artist_choice
+    ) |> 
+      pull(hex_code)
+    
+    top_songs <- merged_kpop_hits_all_years |> 
+      filter(artist.s == artist)
+    
+    song_asp <- switch(
+      input$song_aspect,
+      "Danceability" = top_songs$danceability,
+      "Loudness" = top_songs$loudness,
+      "Energy" = top_songs$energy,
+      "Acousticness" = top_songs$acousticness,
+      "Mode" = top_songs$mode,
+      "Speechiness" = top_songs$speechiness,
+      "Tempo" = top_songs$tempo,
+      "Duration (ms)" = top_songs$duration_ms
+    )
+    
+    top_songs |> 
+      ggplot(aes(year, song_asp)) +
+      geom_point() +
+      geom_smooth(se = FALSE, color = artist_color) + 
+      scale_x_continuous(labels = function(x) paste0(x)) +  
+      labs(y = NULL,
+           x = NULL) +  
+      theme_minimal()
+    
+    
+  })
+  
+  
   
 }
 
